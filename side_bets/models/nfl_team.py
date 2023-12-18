e0753475c269722a7728b99e941379dd64367ce1
Michael Gunn <mijogu@gmail.com>
2023-12-18 18:50:53 -0500

    add fields to nfl_team

--- side_bets/models/nfl_team.py
+++ side_bets/models/nfl_team.py
@@ -4,6 +4,8 @@ class NFLTeam(models.Model):
     city = models.CharField(max_length=100)
     name = models.CharField(max_length=100, unique=True)
     short_name = models.CharField(max_length=10, unique=True)
+    conference = models.CharField(max_length=3, null=True)
+    division = models.CharField(max_length=5, null=True)
 
     # relationships
     # players = many-to-one rel from NFLPlayer
